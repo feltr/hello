7f6a0bfd1b7582df4c3d1998274f34e19117a595
feltr <dn_fedorovtsev@mail.ru>
2021-11-07 00:41:16 +0500

    project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace visual_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = "as";
+            Console.WriteLine(a);
+        }
+    }
+}
